737819dd3e58ea440dab59ecec7b795c7dda3476
WAHID BUX K22SW <wahidbux>
2025-07-21 17:28:18 +0500

    mongodb connected

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import dotenv from 'dotenv';
-dotenv.config();
-import express from "express";
-const app = express()
-const port = 3000
+import connectDB from './db/db_connection.js';
 
-app.get('/',(req ,res )=>{
-    res.send("Hey, How are you.......");
-})
-app.get('/home',(req , res)=>{
-    res.send("Hey, This is your home page");
-})
-app.get('/login',(req,res)=>{
-    res.send('<h1>Please login your account here...<h1/>')
-})
-app.listen(process.env.port , ()=>{
-    console.log(`Server is running on ${port}`);
-})
+dotenv.config({
+     path: './public/temp/.env'
+});
+
+connectDB();
+
+
+
+
+
+// app.get('/',(req ,res )=>{
+//     res.send("Hey, How are you.......");
+// })
+// app.get('/home',(req , res)=>{
+//     res.send("Hey, This is your home page");
+// })
+// app.get('/login',(req,res)=>{
+//     res.send('<h1>Please login your account here...<h1/>')
+// })
+// app.listen(port, ()=>{
+//     console.log(`Server is running on ${port}`);
+// })
